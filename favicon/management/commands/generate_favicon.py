d71ee5b0ccaa358a194c7526003935b205e4b171
ZuluPro <montheanthony@hotmail.com>
2016-04-21 18:19:51 -0400

    Added frontend part

--- favicon/management/commands/generate_favicon.py
+++ favicon/management/commands/generate_favicon.py
@@ -1,9 +1,16 @@
 import os
 from django.core.management.base import BaseCommand, CommandError
+from django.template.loader import get_template
 from PIL import Image
 from favicon import settings
 
-PNG_SIZE = (32, 57, 76, 96, 120, 128, 144, 152, 180, 195, 196, 228, 270, 558)
+PNG_SIZE = (32, 57, 76, 96, 120, 128, 144, 152, 180, 195, 196)
+WINDOWS_PNG_SIZE = (
+    ([128, 128], 'smalltile.png'),
+    ([270, 270], 'mediumtile.png'),
+    ([558, 270], 'widetile.png'),
+    ([558, 558], 'largetile.png'),
+)
 
 
 class Command(BaseCommand):
@@ -25,3 +32,14 @@ class Command(BaseCommand):
             output = os.path.join(settings.STATIC_DIR, 'favicon-%s.png' % size)
             img.thumbnail(size=(size, size), resample=Image.ANTIALIAS)
             img.save(output)
+        for size, filename in PNG_SIZE:
+            img = Image.open(filename)
+            output = os.path.join(settings.STATIC_DIR, 'filename')
+            img.thumbnail(size=size, resample=Image.ANTIALIAS)
+            img.save(output)
+        # Create ieconfig.xml
+        output = os.path.join(settings.STATIC_DIR, 'ieconfig.xml')
+        with open(output, 'w') as output_file:
+            template = get_template('favicon/ieconfig.xml')
+            output_content = template.render({'tile_color': 'FFFFFF'})
+            output_file.write(output_content)
