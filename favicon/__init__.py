49ae79c4998038bf70dce057289872699bdd105a
Basil Shubin <basil.shubin@gmail.com>
2016-06-29 16:17:44 +0700

    Update __init__.py
    fixed wrong url

--- favicon/__init__.py
+++ favicon/__init__.py
@@ -8,4 +8,4 @@ VERSION = (0, 6, 0)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Anthony Monthe (ZuluPro)'
 __email__ = 'anthony.monthe@gmail.com'
-__url__ = 'https://github.com/ZuluPro/favicon'
+__url__ = 'https://github.com/ZuluPro/django-super-favicon'
