381bcc37d1b63bb8422b925bd269dda3638006ed
Ryan Kaiser <ryandkaiser@gmail.com>
2016-07-15 22:37:31 -0500

    add optional setting for background color of apple-precomposed icons

--- favicon/settings.py
+++ favicon/settings.py
@@ -4,3 +4,4 @@ from django.conf import settings
 
 STORAGE = getattr(settings, 'FAVICON_STORAGE', settings.STATICFILES_STORAGE)
 STORAGE_OPTIONS = getattr(settings, 'FAVICON_STORAGE_OPTIONS', {})
+PRECOMPOSED_BGCOLOR = getattr(settings, 'FAVICON_PRECOMPOSED_BG_COLOR', (255, 255, 255))
